e94acb8f137e32df9f8ffb68a4f682795b0ce070
Mick210 <sid222m@gmail.com>
2023-11-05 23:37:03 +0300

    commit

--- src/main/java/com/example/HW_2_12_2/exeption/DivideByZeroException.java
+++ src/main/java/com/example/HW_2_12_2/exeption/DivideByZeroException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class DivideByZeroException extends IllegalArgumentException{
+public class DivideByZeroException extends IllegalArgumentException {
     public DivideByZeroException(String s) {
         super(s);
     }
